a5f160e80ff409b43ff1ee32f3e772e77f9cf10e
BerkanSezer <kberkansezer@gmail.com>
2018-05-18 09:44:37 +0300

    README.md and package.json arechanged

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 $( document ).ready(function() {
+  $(".page").hide();
   $("#page-1").show();
-  $("#page-2").hide();
-  $("#page-3").hide();
-  $("#page-4").hide();
-  $("#page-5").hide();
-  $("#page-6").hide();
   var pageCounter = 1;
 
   $(".next-button").on("click",function(){
@@ -22,38 +18,7 @@ $( document ).ready(function() {
     }
   });
   function pageSwicher(pageCounter) {
-    switch (pageCounter){
-      case 1:
-        $("#page-2").hide();
-        $("#page-1").show();
-        break;
-      case 2:
-        $("#page-1").hide();
-        $("#page-3").hide();
-        $("#page-2").show();
-        break;
-      case 3:
-        $("#page-2").hide();
-        $("#page-4").hide();
-        $("#page-3").show();
-        break;
-      case 4:
-        $("#page-3").hide();
-        $("#page-5").hide();
-        $("#page-4").show();
-        break;
-      case 5:
-        $("#page-4").hide();
-        $("#page-6").hide();
-        $("#page-5").show();
-        break;
-      case 6:
-        $("#page-5").hide();
-        $("#page-6").show();
-        break;
-    }
+    $(".page").hide();
+    $("#page-" + pageCounter).show();
   }
-
-
-
 });
