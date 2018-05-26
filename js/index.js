3b10b505e1270bb1eea531d064f5eaa04cc94b32
BerkanSezer <kberkansezer@gmail.com>
2018-05-26 22:20:33 +0300

    j-query UI is added

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
   var pageCounter = 1;
 
   $(".next-button").on("click",function(){
-    if (pageCounter !== 6){
+    if (pageCounter !== 10){
       pageCounter ++;
       pageSwicher(pageCounter);
     }
@@ -21,4 +21,29 @@ $( document ).ready(function() {
     $(".page").hide();
     $("#page-" + pageCounter).show();
   }
+  
+  function dragDropGame() {
+    var $app = $('#page-7');
+    console.log('here');
+    console.log(pageCounter);
+    console.log(pageCounter);
+
+    var $orgBox = $app.find('.organelles');
+    var organelles = ['Ribozom', 'Mitokondri', 'Golgi Cisimciği', 'Endoplazmik retikulum', 'Koful', 'Lizozom', 'Hücre zarı', 'Çekirdek', 'Periksizom', 'Plastitler', 'Hücre Çeperi', 'Sentrozom'];
+    for(var i=1;i<=12;i++){
+      var data = '<div class="col-sm-2" id="org_'+ i+'">'+organelles[i-1]+'</div>';
+      $orgBox.append(data)
+    }
+    $('.organelles').children().draggable();
+    $app.find('.circle-a').droppable({
+      drop: function() {
+        $(this)
+          .css("background-color", "yellow");
+      }
+    });
+  }
+    dragDropGame();
+
+
+  
 });
